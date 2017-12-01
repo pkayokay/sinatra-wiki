7cc2be780657b67dbe26ce45e151b0693d97b350
Paul Kim <pkayokay@gmail.com>
2017-12-01 15:23:07 -0500

    edit on existing page

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+# Sinatra Wiki Page
+
 1. `gem install sinatra`
 2. `ruby wiki.rb` on terminal
--- wiki.rb
+++ wiki.rb
@@ -21,14 +21,23 @@ get '/new' do
   erb :new
 end
 
+
 get '/:title' do
   @title = params[:title]
   @content = page_content(@title)
   erb :show
 end
 
+get '/:title/edit' do
+  @title = params[:title]
+  @content = page_content(@title)
+  erb :edit
+end
+
 post '/create' do
   save_content(params['title'], params['content'])
   redirect URI.escape("/#{params['title']}")
 end
+
+
 
