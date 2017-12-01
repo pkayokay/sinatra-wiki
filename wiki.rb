6a5cabf1d7a4463cd9d27791659c9bed10de94ab
Paul Kim <pkayokay@gmail.com>
2017-12-01 16:33:02 -0500

    delete pages

--- wiki.rb
+++ wiki.rb
@@ -13,6 +13,10 @@ def save_content(title, content)
   end
 end
 
+def delete_content(title)
+  File.delete("pages/#{title}.txt")
+end
+
 get '/' do
   erb :welcome
 end
@@ -42,6 +46,11 @@ end
 put '/:title' do
   save_content(params['title'], params['content'])
   redirect URI.escape("/#{params['title']}")
+end
+
+delete '/:title' do
+  delete_content(params[:title])
+  redirect '/'
 end
 
 
